914318d4f7e08f8b78999457c7eac129bedff1df
Miguel Rocha <mrocha1095@gmail.com>
2022-09-10 23:24:01 -0400

    13commit

--- routes/goals.js
+++ routes/goals.js
@@ -25,7 +25,7 @@ router.post("/create", isAuthenticated, async (req, res) => {
     let newGoal = await Goal.create({
       title: req.body.title,
       date: req.body.date,
-      current: req.body.current,
+      current: 0,
       amount: req.body.amount,
       creatorId: req.user.id,
     });
